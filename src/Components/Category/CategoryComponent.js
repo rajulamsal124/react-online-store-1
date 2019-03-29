817726a9a36af2a362898cd261cc1c1c20391082
Mahmoud <mhany95@hotmail.com>
2019-03-30 00:58:23 +0200

    -Added product category components

---
+++ src/Components/Category/CategoryComponent.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import HelperManager from "../../Managers/HelperManager";
+import ProductsManger from "../../Managers/ProductsManager";
+
+let cardStyle = {
+  width: "100%"
+};
+
+let cardMediaStyle = {
+  height: "150px"
+};
+
+let cardContentStyle = {
+  textAlign: "center",
+  height: "45px",
+  paddingTop: "10px",
+  fontWeight: "500"
+};
+
+export default class CategoryComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.category = this.props.category;
+  }
+
+  handleCategoryClick = () => {
+    ProductsManger.setCurrentlySelectedCategory(this.category);
+  };
+
+  render() {
+    return (
+      <Card raised style={cardStyle} onClick={this.handleCategoryClick}>
+        <CardActionArea>
+          <CardMedia
+            image={HelperManager.getImage(this.category)}
+            style={cardMediaStyle}
+          />
+          <CardContent style={cardContentStyle}>
+            {this.category.name}
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    );
+  }
+}
+
+CategoryComponent.propTypes = {
+  category: PropTypes.object.isRequired
+};
---
+++ src/Components/Category/CategoryContainer.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
+import CategoryComponent from "./CategoryComponent.js";
+
+let containerStyle = {
+  padding: "2%"
+};
+
+let headerStyle = {
+  textAlign: "center",
+  marginBottom: "2%"
+};
+export default class CategoryContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://test-api.edfa3ly.io/category")
+      .then(response => response.json())
+      .then(data => {
+        let categoriesArray = [];
+
+        //populate array with results
+        data.map(obj =>
+          categoriesArray.push(
+            <Col>
+              <CategoryComponent category={obj} />
+            </Col>
+          )
+        );
+
+        //set state one time only
+        this.setState({
+          categories: categoriesArray
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <div style={containerStyle}>
+        <h1 style={headerStyle}>Please choose a product category</h1>
+        <Row>{this.state.categories}</Row>
+      </div>
+    );
+  }
+}
