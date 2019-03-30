391ee2ac4e6ab3161291f4697080a99f7edf6521
Mahmoud <mhany95@hotmail.com>
2019-03-30 23:21:22 +0200

    -Added a color filter component

---
+++ src/Components/Filter/ColorFilter.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import ProductsManager from "../../Managers/ProductsManager";
+import HelperManager from "../../Managers/HelperManager";
+
+let containerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  marginLeft: "10%",
+  marginRight: "10%",
+  marginTop: "5%"
+};
+
+export default class ColorFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorList: [],
+      selectedColor: "",
+      open: false
+    };
+    ProductsManager.setRefreshColorFilterComponent(this.refreshColorList);
+  }
+
+  handleChange = event => {
+    this.setState({ selectedColor: event.target.value });
+    ProductsManager.addFilter("color", event.target.value);
+    ProductsManager.executeFetchingFilteredProductsTrigger();
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  refreshColorList = list => {
+    this.setState({
+      colorList: list
+    });
+  };
+
+  renderColor = () => {
+    if (this.state.colorList !== []) {
+      let arr = [];
+      arr.push(
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+      );
+
+      this.state.colorList.map(color =>
+        arr.push(
+          <MenuItem value={color}>
+            {HelperManager.capitalizeFirstLetter(color)}
+          </MenuItem>
+        )
+      );
+
+      return arr;
+    }
+    return [];
+  };
+
+  render() {
+    if (this.state.colorList.length !== 0)
+      return (
+        <div style={containerStyle}>
+          <InputLabel htmlFor="demo-controlled-open-select">Color</InputLabel>
+          <Select
+            open={this.state.open}
+            onClose={this.handleClose}
+            onOpen={this.handleOpen}
+            value={this.state.selectedColor}
+            onChange={this.handleChange}
+          >
+            {this.renderColor()}
+          </Select>
+        </div>
+      );
+
+    return <div />;
+  }
+}
--- src/Managers/ProductsManager.js
+++ src/Managers/ProductsManager.js
@@ -2,6 +2,13 @@ let currentlySelectedCategory = undefined;
 let fetchingProductsTrigger = undefined;
 let loadingTigger = undefined;
 
+let refreshPriceFilterComponent = undefined;
+let refreshColorFilterComponent = undefined;
+let refreshRatingFilterComponent = undefined;
+
+let fetchingFilteredProductsTrigger = undefined;
+let filter = {};
+
 class ProductsManager {
   //set a callback to the loading trigger in products container
   setLoadingTrigger = callback => {
@@ -28,8 +35,13 @@ class ProductsManager {
 
   //set the currently selected category
   //will execute product fetching
+  //and clears any previously set filters
   setCurrentlySelectedCategory = category => {
     currentlySelectedCategory = category;
+    this.clearFilter(); //clear previous filters
+    this.executeRefreshPriceFilterComponent(undefined, undefined); //clear previously set price filter values
+    this.executeRefreshColorFilterComponent([]); //clear previously set color filter values
+    this.executeRefreshRatingFilterComponent([]); //clear previously set rating filter values
     this.executeFetchingProductsTrigger();
   };
 
@@ -38,6 +50,77 @@ class ProductsManager {
     if (currentlySelectedCategory !== undefined)
       return String().concat("?categoryId=", currentlySelectedCategory.id);
     return "";
+  };
+
+  //used to set a trigger to refresh the PriceFilter component
+  setRefreshPriceFilterComponent = callback => {
+    refreshPriceFilterComponent = callback;
+  };
+
+  //this method takes the lowest and highest price found when a user chooses a category
+  executeRefreshPriceFilterComponent = (min, max) => {
+    if (refreshPriceFilterComponent !== undefined)
+      refreshPriceFilterComponent(min, max);
+  };
+
+  //used to set a trigger to refresh the ColorFilter component
+  setRefreshColorFilterComponent = callback => {
+    refreshColorFilterComponent = callback;
+  };
+
+  //this method takes a list of retrieved colors when a user chooses a category
+  executeRefreshColorFilterComponent = listOfColors => {
+    if (refreshColorFilterComponent !== undefined)
+      refreshColorFilterComponent(listOfColors);
+  };
+
+  //used to set a trigger to refresh the RatingFilter component
+  setRefreshRatingFilterComponent = callback => {
+    refreshRatingFilterComponent = callback;
+  };
+
+  //this method takes a list of retrieved ratings when a user chooses a category
+  executeRefreshRatingFilterComponent = listOfRatings => {
+    if (refreshRatingFilterComponent !== undefined)
+      refreshRatingFilterComponent(listOfRatings);
+  };
+
+  //set a callback to the filtered product fetching method
+  setFetchingFilteredProductsTrigger = callback => {
+    fetchingFilteredProductsTrigger = callback;
+  };
+
+  //execute filtered product fetching method
+  //filteration is done based on an already selected product category
+  executeFetchingFilteredProductsTrigger = () => {
+    if (fetchingFilteredProductsTrigger !== undefined) {
+      let filterString = this.constructFilterString();
+      //if (filterString !== "") {
+      this.executeLoadingTrigger(true);
+      fetchingFilteredProductsTrigger(filterString);
+      //}
+    }
+  };
+
+  //used to add any filters
+  addFilter = (key, value) => {
+    if (value === "") {
+      //unsetting a filter
+      delete filter[key];
+    } else filter[key] = value;
+  };
+
+  clearFilter = () => {
+    filter = {};
+  };
+
+  //used to create one string to be added to the url of the request
+  constructFilterString = () => {
+    let filterString = "";
+    for (let key in filter) {
+      filterString += String().concat("&", key, "=", filter[key]);
+    }
+    return filterString;
   };
 }
 
