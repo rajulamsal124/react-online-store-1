5db3a74373d40822c3f404b3ad1d4518a8a7c8f8
Mahmoud <mhany95@hotmail.com>
2019-03-30 23:21:37 +0200

    -Added a rating filter component

---
+++ src/Components/Filter/RatingFilter.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import ProductsManager from "../../Managers/ProductsManager";
+import HelperManager from "../../Managers/HelperManager";
+
+let containerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  marginLeft: "10%",
+  marginRight: "10%",
+  marginTop: "5%"
+};
+
+export default class RatingFilter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ratingsList: [],
+      selectedRating: "",
+      open: false
+    };
+    ProductsManager.setRefreshRatingFilterComponent(this.refreshRatingsList);
+  }
+
+  handleChange = event => {
+    this.setState({ selectedRating: event.target.value });
+    ProductsManager.addFilter("rating", event.target.value);
+    ProductsManager.executeFetchingFilteredProductsTrigger();
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  refreshRatingsList = list => {
+    this.setState({
+      ratingsList: list
+    });
+  };
+
+  renderRating = () => {
+    if (this.state.ratingsList !== []) {
+      let arr = [];
+      arr.push(
+        <MenuItem value="">
+          <em>None</em>
+        </MenuItem>
+      );
+
+      this.state.ratingsList.map(rating =>
+        arr.push(<MenuItem value={rating}>{rating}</MenuItem>)
+      );
+
+      return arr;
+    }
+    return [];
+  };
+
+  render() {
+    if (this.state.ratingsList.length !== 0)
+      return (
+        <div style={containerStyle}>
+          <InputLabel htmlFor="demo-controlled-open-select">Rating</InputLabel>
+          <Select
+            open={this.state.open}
+            onClose={this.handleClose}
+            onOpen={this.handleOpen}
+            value={this.state.selectedRating}
+            onChange={this.handleChange}
+          >
+            {this.renderRating()}
+          </Select>
+        </div>
+      );
+
+    return <div />;
+  }
+}
